1c9ef39170f129e31afa6bdfab2b6c90088b5bf8
Mikel Gascon <mikelgascon@outlook.com>
2019-11-13 23:03:24 +0000

    query now has text and values to prevent SQL injections

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,18 +7,25 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-let userInput = process.argv.slice(2)
+const userInput = process.argv.slice(2)
+const cohortName = userInput[0]
+const limit = userInput[1]
 
 
-pool.query(`
-SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-FROM students
-JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${userInput[0]}%'
-LIMIT ${userInput[1]};
-`)
-.then(res => {
-  res.rows.forEach(user => {
-    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
-  })
+const query = {
+  text: 
+    `SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+    FROM students
+    JOIN cohorts ON cohorts.id = cohort_id
+    WHERE cohorts.name LIKE $1
+    LIMIT $2;`,
+  values: [`%${cohortName}%`, limit]
+};
+
+pool
+  .query(query)
+  .then(res => {
+    res.rows.forEach(user => {
+      console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+    })
 });
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,19 +7,25 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-let userInput = process.argv.slice(2)
+const userInput = process.argv.slice(2)
+const cohortName = userInput[0]
 
-pool.query(`
-SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
-FROM teachers
-JOIN assistance_requests ON teacher_id = teachers.id
-JOIN students ON student_id = students.id
-JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${userInput[0]}'
-ORDER BY teacher;
-`)
-.then(res => {
-  res.rows.forEach(row => {
-    console.log(`${row.cohort}: ${row.teacher}`);
-  })
+const query = {
+  text: 
+    `SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+    FROM teachers
+    JOIN assistance_requests ON teacher_id = teachers.id
+    JOIN students ON student_id = students.id
+    JOIN cohorts ON cohort_id = cohorts.id
+    WHERE cohorts.name LIKE $1
+    ORDER BY teacher;`,
+  values:[`%${cohortName}%`]
+} 
+
+pool
+  .query(query)
+  .then(res => {
+    res.rows.forEach(row => {
+      console.log(`${row.cohort}: ${row.teacher}`);
+    })
 });
